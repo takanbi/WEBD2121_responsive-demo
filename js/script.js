64823a44212530264167be5c02ed2e6125343634
takanbi <tunji.akanbi@century.edu>
2022-12-08 13:46:33 -0600

    issues with bootstrap navigation, temporary fix

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // $('.nav-link').on('click', ()=> {
+    //     $('.navbar-toggler').removeClass('is-active');//reversed hamburger js
+    //     $('.navbar-toggler').addClass('collapsed');//reversed bootstrap js
+    //     $('.navbar-collapse').removeClass('show');//reversed bootstrap js
+    //   });
+
+
+      //document.ready ends here  
+  });
